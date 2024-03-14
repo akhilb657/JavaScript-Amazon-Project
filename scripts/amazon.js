cd9daad4632c64459ecc4c93f6f363d738999c8c
akhil <akhilboyanapalli657@gmail.com>
2024-03-14 14:06:57 -0500

    cart Quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,8 +79,16 @@ document.querySelector('.js-products-grid')
           cart.push({
             productId: productId,
             quantity:1
-        })
+        });
       }
-        console.log(cart);
+
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity')
+          .innerHTML = cartQuantity;
       });
     })
